8ded36bc6c5eda4eebf5745af6f9800b647ca11c
Mike Carmody <mcarmody2013@gmail.com>
2023-04-19 13:06:35 -0400

    Update main.js

--- modules/scripts/main.js
+++ modules/scripts/main.js
@@ -1,22 +1,33 @@
-let config;
 let websocket;
 
 Hooks.on("init", async () => {
-  config = await game.packs.get("foundry-discord-chat.config").getContent();
-  const serverIp = config[0].data.server_ip;
-  websocket = new WebSocket(`${serverIp.replace('http', 'ws')}`);
-
-  websocket.onopen = () => {
-    console.log('Connected to WebSocket server');
-  };
-
-  game.socket.on("module.foundry-discord-chat", async (data) => {
-      try {
-        websocket.send(JSON.stringify({ message: data.message }));
-      } catch (error) {
-        console.error('Error sending message to Discord bot:', error);
-      }
+  game.settings.register("foundry-discord-chat", "serverIp", {
+    name: "Discord Bot Server IP",
+    hint: "Enter the IP address and port where the Discord bot is running (e.g., http://192.168.1.10:3000)",
+    scope: "world",
+    config: true,
+    default: "",
+    type: String,
   });
+
+  const serverIp = game.settings.get("foundry-discord-chat", "serverIp");
+  if (serverIp) {
+    websocket = new WebSocket(`${serverIp.replace('http', 'ws')}`);
+
+    websocket.onopen = () => {
+      console.log('Connected to WebSocket server');
+    };
+
+    game.socket.on("module.foundry-discord-chat", async (data) => {
+      if (game.user.isGM) {
+        try {
+          websocket.send(JSON.stringify({ message: data.message }));
+        } catch (error) {
+          console.error('Error sending message to Discord bot:', error);
+        }
+      }
+    });
+  }
 });
 
 Hooks.on("chatMessage", async (log, message, chatData) => {
