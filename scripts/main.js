3edf8f59723131c4489e9be68fe4dbc65cf80656
Mike Carmody <mcarmody2013@gmail.com>
2023-04-30 18:48:57 -0400

    build

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+Hooks.once("init", async function () {
+  console.log("your-module-name | Initializing your-module-name");
+
+  // Add this line to start the API server
+  require("./apiServer");
+}
+
 Hooks.on("chatMessage", async (log, message, chatData) => {
   game.socket.emit("module.foundry-discord-chat", { message: chatData.content });
 });
