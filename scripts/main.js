ab913467b2a79c2bd1c11ecd3b003fe649bb4f24
Mike Carmody <mcarmody2013@gmail.com>
2023-04-30 20:24:20 -0400

    emit test message

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,17 @@ Hooks.once("init", async function () {
   console.log("Discord Chat | Initializing discord bot");
 
   // Add this line to start the API server
-  require("./apiServer");
 })
 
 Hooks.once("ready", async function () {
-  // Other ready hook code ...
+  console.log("Ready!");
+
+  // Emit a test message
+  game.socket.emit("module.foundry-discord-chat", { operation: "testMessage", message: "Foundry VTT module is ready!" });
 
   // Set up a custom socket event for fetching character data
-  game.socket.on("module.your-module-name", async (data, callback) => {
+  game.socket.on("module.foundry-discord-chat", async (data, callback) => {
+    console.log("Received request in Foundry VTT module:", data);
     if (data.operation === "fetchCharacterData") {
       const characterName = data.characterName;
 
@@ -17,14 +20,12 @@ Hooks.once("ready", async function () {
       const characters = game.actors.filter((actor) => actor.data.type === "character" && actor.name.toLowerCase() === characterName.toLowerCase());
 
       if (characters.length > 0) {
+        console.log("Found character in Foundry VTT:", characters[0].data);
         callback(characters[0].data);
       } else {
+        console.log("Character not found in Foundry VTT");
         callback(null);
       }
     }
   });
-});
-
-Hooks.on("chatMessage", async (log, message, chatData) => {
-  game.socket.emit("module.foundry-discord-chat", { message: chatData.content });
 });
