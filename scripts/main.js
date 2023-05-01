09d86628c846c99d386c4fa1e3494312821c0ada
Mike Carmody <mcarmody2013@gmail.com>
2023-05-01 14:34:36 -0400

    update socket order again

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,27 @@ Hooks.once("init", async function () {
 
 Hooks.once("ready", async function () {
   console.log("Ready!");
+  // Set up a custom socket event for fetching character data
+  game.socket.on("module.foundry-discord-chat", async (data, callback) => {
+    console.log("Received request in Foundry VTT module:", data);
+    if (data.operation === "fetchCharacterData") {
+      const characterName = data.characterName;
+
+      // Search for the character in Foundry VTT using the Foundry API
+      const characters = game.actors.filter((actor) => actor.data.type === "character" && actor.name.toLowerCase() === characterName.toLowerCase());
+
+      if (characters.length > 0) {
+        console.log("Found character in Foundry VTT:", characters[0].data);
+        callback(characters[0].data);
+      } else {
+        console.log("Character not found in Foundry VTT");
+        callback(null);
+      }
+    } else if (data.operation === "rollTest") {
+      // Log the test in Foundry VTT or perform any other action.
+      console.log("Test rolled:", data.test);
+    }
+  });
 
   // Emit a test message
   game.socket.emit("module.foundry-discord-chat", { operation: "testMessage", message: "Foundry VTT module is ready!" });
@@ -18,26 +39,5 @@ Hooks.once("ready", async function () {
     const characters = folder.content.filter((actor) => actor.data.type === "character");
     console.log("Emitting characters: "+characters);
     game.socket.emit("module.foundry-discord-chat", { operation: "characterList", characters: characters.map((c) => c.data) });
-  }
-});
-// Set up a custom socket event for fetching character data
-game.socket.on("module.foundry-discord-chat", async (data, callback) => {
-  console.log("Received request in Foundry VTT module:", data);
-  if (data.operation === "fetchCharacterData") {
-    const characterName = data.characterName;
-
-    // Search for the character in Foundry VTT using the Foundry API
-    const characters = game.actors.filter((actor) => actor.data.type === "character" && actor.name.toLowerCase() === characterName.toLowerCase());
-
-    if (characters.length > 0) {
-      console.log("Found character in Foundry VTT:", characters[0].data);
-      callback(characters[0].data);
-    } else {
-      console.log("Character not found in Foundry VTT");
-      callback(null);
-    }
-  } else if (data.operation === "rollTest") {
-    // Log the test in Foundry VTT or perform any other action.
-    console.log("Test rolled:", data.test);
   }
 });
